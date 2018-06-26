e11a418d85dcd686a5b2faba0120c6527ce5fbe6
qiweiqiang10 <1276422345@qq.com>
2018-06-26 13:52:33 +0800

    Add files via upload

---
+++ TestNewR/TestNewR/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  TestNewR
+//
+//  Created by PrimeCloud on 2018/6/22.
+//  Copyright © 2018年 QiZhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)didClickRecordButon:(id)sender;
+- (IBAction)didClickPlaybutton:(id)sender;
+
+
+
+
+@end
+
---
+++ TestNewR/TestNewR/ViewController.m
@@ -0,0 +1,162 @@
+//
+//  ViewController.m
+//  TestNewR
+//
+//  Created by PrimeCloud on 2018/6/22.
+//  Copyright © 2018年 QiZhuo. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+#import "MLAudioRecorder.h"
+#import "CafRecordWriter.h"
+#import "AmrRecordWriter.h"
+#import "Mp3RecordWriter.h"
+#import "MLAudioMeterObserver.h"
+
+#import "MLAudioPlayer.h"
+#import "AmrPlayerReader.h"
+
+@interface ViewController ()<AVAudioPlayerDelegate,AVAudioRecorderDelegate>
+
+
+@property (nonatomic, strong) MLAudioRecorder *recorder;
+@property (nonatomic, strong) CafRecordWriter *cafWriter;
+@property (nonatomic, strong) AmrRecordWriter *amrWriter;
+@property (nonatomic, strong) Mp3RecordWriter *mp3Writer;
+
+@property (nonatomic, strong) MLAudioMeterObserver *meterObserver;
+
+@property (retain, nonatomic) AVAudioPlayer *audioPlayer;
+
+@property(retain,nonatomic)MLAudioPlayer *mlPlayer;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+
+    CafRecordWriter *writer = [[CafRecordWriter alloc]init];
+    writer.filePath = [path stringByAppendingPathComponent:@"record.caf"];
+    self.cafWriter = writer;
+
+    AmrRecordWriter *amrWriter = [[AmrRecordWriter alloc]init];
+    amrWriter.filePath = [path stringByAppendingPathComponent:@"record.amr"];
+    amrWriter.maxSecondCount = 60;
+    amrWriter.maxFileSize = 1024*256;
+    self.amrWriter = amrWriter;
+
+    Mp3RecordWriter *mp3Writer = [[Mp3RecordWriter alloc]init];
+    mp3Writer.filePath = [path stringByAppendingPathComponent:@"record.mp3"];
+//    mp3Writer.maxSecondCount = 60;
+//    mp3Writer.maxFileSize = 1024*256;
+    mp3Writer.maxSecondCount = 300;
+    mp3Writer.maxFileSize = 1024*256 * 100;
+    self.mp3Writer = mp3Writer;
+
+    MLAudioMeterObserver *meterObserver = [[MLAudioMeterObserver alloc]init];
+    meterObserver.actionBlock = ^(NSArray *levelMeterStates,MLAudioMeterObserver *meterObserver){
+        DLOG(@"volume:%f",[MLAudioMeterObserver volumeForLevelMeterStates:levelMeterStates]);
+    };
+    meterObserver.errorBlock = ^(NSError *error,MLAudioMeterObserver *meterObserver){
+        [[[UIAlertView alloc]initWithTitle:@"错误" message:error.userInfo[NSLocalizedDescriptionKey] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"知道了", nil]show];
+    };
+    self.meterObserver = meterObserver;
+
+    MLAudioRecorder *recorder = [[MLAudioRecorder alloc]init];
+    __weak __typeof(self)weakSelf = self;
+    recorder.receiveStoppedBlock = ^{
+//        [weakSelf.recordButton setTitle:@"Record" forState:UIControlStateNormal];
+        weakSelf.meterObserver.audioQueue = nil;
+    };
+    recorder.receiveErrorBlock = ^(NSError *error){
+//        [weakSelf.recordButton setTitle:@"Record" forState:UIControlStateNormal];
+        weakSelf.meterObserver.audioQueue = nil;
+
+        [[[UIAlertView alloc]initWithTitle:@"错误" message:error.userInfo[NSLocalizedDescriptionKey] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"知道了", nil]show];
+    };
+
+
+    //caf
+//            recorder.fileWriterDelegate = writer;
+    //        self.filePath = writer.filePath;
+    //mp3
+        recorder.fileWriterDelegate = mp3Writer;
+//        self.filePath = mp3Writer.filePath;
+
+    //amr
+//    recorder.bufferDurationSeconds = 0.25;
+//    recorder.fileWriterDelegate = self.amrWriter;
+
+    self.recorder = recorder;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)didClickRecordButon:(id)sender {
+
+    [[AVAudioSession sharedInstance]requestRecordPermission:^(BOOL granted) {
+        if (granted) {
+            NSLog(@"开始录制");
+            [self.recorder startRecording];
+            self.meterObserver.audioQueue = self.recorder->_audioQueue;
+        }
+    }];
+
+
+
+}
+
+- (IBAction)didClickPlaybutton:(id)sender {
+    [self.recorder stopRecording];
+    NSLog(@"%@-------%@",_amrWriter.filePath,_mp3Writer.filePath);
+
+}
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
+    if (_audioPlayer.isPlaying) {
+        [_audioPlayer stop];
+    }
+    long long fileLength = [self fileSizeAtPath:_mp3Writer.filePath];
+    NSLog(@"%lld",fileLength);
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:self.mp3Writer.filePath] error:nil];
+    _audioPlayer.delegate = self;
+    [_audioPlayer play];
+
+
+//    _mlPlayer = [[MLAudioPlayer alloc]init];
+//    AmrPlayerReader *amrReader = [[AmrPlayerReader alloc]init];
+//    _mlPlayer.fileReaderDelegate = amrReader;
+//    _mlPlayer.receiveErrorBlock = ^(NSError *error){
+//
+//        [[[UIAlertView alloc]initWithTitle:@"错误" message:error.userInfo[NSLocalizedDescriptionKey] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"知道了", nil]show];
+//    };
+//    _mlPlayer.receiveStoppedBlock = ^{
+//    };
+//    [_mlPlayer startPlaying];
+
+///var/mobile/Containers/Data/Application/DEB0075A-C521-47C5-AB37-3CD95B938DC2/Documents/record.mp3
+}
+
+- (long long) fileSizeAtPath:(NSString*) filePath{
+    NSFileManager* manager = [NSFileManager defaultManager];
+    if ([manager fileExistsAtPath:filePath]){
+        return [[manager attributesOfItemAtPath:filePath error:nil] fileSize];
+    }
+    return 0;
+}
+
+
+@end
